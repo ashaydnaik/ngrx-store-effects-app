b9cd0cdcdf7efbccadf4c48750a874b4fb501c3c
Ashay Naik <ashaynaik@Ashays-MBP.BigPond>
2018-07-08 09:08:54 +1000

    First Effect

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
-
+import * as fromStore from '../../store';
+import {Store} from "@ngrx/store";
+import {Observable} from "rxjs/Observable";
 @Component({
   selector: 'products',
   styleUrls: ['products.component.scss'],
   templateUrl: 'products.component.html'
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+  pizzas$: Observable<Pizza[]>;
 
-  constructor(private pizzaService: PizzasService) {}
+  constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
-    });
+    this.pizzas$ = this.store.select(fromStore.getAllPizzas);
+    this.store.dispatch(new fromStore.LoadPizzas());
   }
 }

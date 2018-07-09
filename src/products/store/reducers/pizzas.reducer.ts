d66a5bf93c4feed174e8d4f9bf22988707123891
Ashay Naik <ashaynaik@Ashays-MBP.BigPond>
2018-07-09 22:30:55 +1000

    CRUD Operations

--- src/products/store/reducers/pizzas.reducer.ts
+++ src/products/store/reducers/pizzas.reducer.ts
@@ -28,6 +28,17 @@ export function reducer(state = initialState, action: fromPizzas.PizzasAction): 
     case fromPizzas.LOAD_PIZZAS_FAIL: {
       return { ...state, loading: false, loaded: false };
     }
+    case fromPizzas.UPDATE_PIZZA_SUCCESS:
+    case fromPizzas.CREATE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const entities = {...state.entities, [pizza.id]: pizza};
+      return {...state, entities};
+    }
+    case fromPizzas.REMOVE_PIZZA_SUCCESS: {
+      const pizza = action.payload;
+      const {[pizza.id]:removed, ...entities } = state.entities;
+      return {...state, entities};
+    }
   }
   return state;
 }

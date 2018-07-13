bba0938ae36bebe128e277380cc3a61f6ef62c9a
Ashay Naik <ashaynaik@Ashays-MBP.BigPond>
2018-07-14 08:49:52 +1000

    Router Guards

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -13,15 +13,18 @@ import * as fromComponents from './components';
 // containers
 import * as fromContainers from './containers';
 
+// guards
+import * as fromGuards from './guards';
+
 // services
 import * as fromServices from './services';
 import {EffectsModule} from "@ngrx/effects";
 
 // routes
 export const ROUTES: Routes = [
-  { path: '', component: fromContainers.ProductsComponent, },
-  { path: 'new', component: fromContainers.ProductItemComponent, },
-  { path: ':pizzaId', component: fromContainers.ProductItemComponent, },
+  { path: '', canActivate: [fromGuards.PizzasGuard], component: fromContainers.ProductsComponent, },
+  { path: 'new', canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard], component: fromContainers.ProductItemComponent, },
+  { path: ':pizzaId', canActivate: [fromGuards.PizzaExistsGuard, fromGuards.ToppingsGuard], component: fromContainers.ProductItemComponent, },
 ];
 
 @NgModule({
@@ -33,7 +36,7 @@ export const ROUTES: Routes = [
     StoreModule.forFeature('products', reducers),
     EffectsModule.forFeature(effects)
   ],
-  providers: [...fromServices.services],
+  providers: [...fromServices.services, ...fromGuards.guards],
   declarations: [...fromContainers.containers, ...fromComponents.components],
   exports: [...fromContainers.containers, ...fromComponents.components],
 })
